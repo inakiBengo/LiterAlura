5843cc0244aad21150cfcc02757b94b1dbaa32d5
inaki Bengeochea <bengoinaki@gmail.com>
2024-05-28 18:58:30 -0300

    API consumption class created and its method too

---
+++ src/main/java/com/challenge/literAlura/ConsumeApi.java
@@ -0,0 +1,25 @@
+package com.challenge.literAlura;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumeApi {
+
+    public String getData (String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response;
+        try {
+            response =client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException err) {
+            throw new RuntimeException(err);
+        }
+        return response.body();
+    }
+}
