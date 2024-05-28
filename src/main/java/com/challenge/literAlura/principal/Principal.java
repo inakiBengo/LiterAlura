41972d5196714bb302d207cbbe6e82d655ad71cc
inaki Bengeochea <bengoinaki@gmail.com>
2024-05-28 18:15:35 -0300

    main method added

--- src/main/java/com/challenge/literAlura/principal/Principal.java
+++ src/main/java/com/challenge/literAlura/principal/Principal.java
@@ -1,4 +1,18 @@
 package com.challenge.literAlura.principal;
 
 public class Principal {
+
+    public void showMenu () {
+        var menu = """
+                Elija la opción ingresando un numero:
+                1- Buscar libro por titulo
+                2- Listar libros registrados
+                3- Listar autores registrados
+                4- Listar autores vivos en un determinado año
+                5- Listar libros por idioma
+                
+                0- Salir
+                """;
+
+    }
 }
